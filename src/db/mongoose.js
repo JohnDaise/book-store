5d4b459f49c3f7706a863920e10350f78d6802af
JohnDaise <jdaise@gmail.com>
2019-12-13 00:47:26 -0500

    basic mongoose setup

--- src/db/mongoose.js
+++ src/db/mongoose.js
@@ -1,2 +1,28 @@
 const mongoose = require('mongoose')
 const validator = require('validator')
+
+mongoose.connect('mongodb://127.0.0.1:27017/book-store', {
+    useNewUrlParser: true,
+    useCreateIndex: true
+})
+
+const Book = mongoose.model('Book', {
+    title: {
+        type: String,
+        required: true, 
+        trim: true
+    },
+    author: {
+        type: String,
+        default: 'N/A',
+        trim: true
+    },
+    description: {
+        type: String,
+        trim: true
+    },
+    price: {
+        type: Number,
+        default: 0
+    }  
+})
